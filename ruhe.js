4d9d7549d0681f8114521581c8e062c0e5d9c12c
Lukas S <SchalkLukas20@gmail.com>
2017-04-23 15:44:53 +0200

    Update ruhe.js

--- ruhe.js
+++ ruhe.js
@@ -1,9 +1,8 @@
-//SinusBot Version 0.9.16 Windows
 //Plugin erstellt von Laki
 //Credit me if you use this Plugin!
 registerPlugin({
-	name: 'Ruhe Script (Neue API)',
-	version: '1.0',
+	name: 'Ruhe Script',
+	version: '1.1',
 	description: 'Bot anschreiben mit einem festlegbaren Command um die Servergruppe Ruhe zu bekommen oder zu entfernen',
 	author: 'Laki <lakinator.bplaced.net>',
 	vars: {
@@ -14,6 +13,10 @@ registerPlugin({
 		command: {
 			title: 'Command auf den der Bot reagiert (Groß und Kleinschreibung ist egal)',
 			type: 'string'
+		},
+		na_groups: {
+			title: 'Nicht erlaubte Servergruppen IDs, ohne Leerzeichen mit Komma getrennt',
+			type: 'string'
 		}
 	}
 }, function(sinusbot, config) {
@@ -22,28 +25,64 @@ registerPlugin({
 	var backend = require("backend");
 	var format = require("format");
 
-	var cmd = config.command.toUpperCase();
+	var cmd = config.command.toUpperCase().trim();
 	var groupID = ""+config.servergroupID;
-	var hasGroup;
 
 	event.on("chat", function(ev) {
-		hasGroup = false;
+		
+		if (ev.text.toUpperCase().trim() == cmd) {
 
-		if (ev.text.toUpperCase() == cmd) {
-
-			ev.client.getServerGroups().forEach(function(group) {
-				if (group.id() == groupID) {
+			if (!hasNotAllowedServerGroup(ev.client)) {
+				if (!hasGroup(ev.client, groupID)) {
+					ev.client.addToServerGroup(groupID);
+					ev.client.chat("\n\t\t\t\tDu hast jetzt die Gruppe " + format.color(backend.getServerGroupByID(groupID).name(), "#0668d8") + format.color(" bekommen", "#02e54e") + "\n\t\t\t\tDu wirst nun nicht mehr genervt auf dem TeamSpeak\n\t\t\t\tSchreibe den Bot erneut an um die Gruppe wieder zu entfernen");
+				} else {
 					ev.client.removeFromServerGroup(groupID);
 					ev.client.chat("\n\t\t\t\tDu wurdest von der Gruppe " + format.color(backend.getServerGroupByID(groupID).name(), "#0668d8") + format.color(" entfernt", "#d60808") + "\n\t\t\t\tSchreibe den Bot erneut an um die Gruppe wieder zu bekommen");
-					hasGroup = true;
-				}
-        	});
-
-			if (!hasGroup) {
-				ev.client.addToServerGroup(groupID);
-				ev.client.chat("\n\t\t\t\tDu hast jetzt die Gruppe " + format.color(backend.getServerGroupByID(groupID).name(), "#0668d8") + format.color(" bekommen", "#02e54e") + "\n\t\t\t\tDu wirst nun nicht mehr genervt auf dem TeamSpeak\n\t\t\t\tSchreibe den Bot erneut an um die Gruppe wieder zu entfernen");
+				}	
 			}
+
+
 		}
+		
 	});
+
+	function hasGroup(cl, groupID) {
+		var group;
+
+		for (var index in cl.getServerGroups()) {
+			group = cl.getServerGroups()[index];
+			if(group.id() == groupID) return true;
+		}
+			
+		return false;
+	}
+
+	function hasNotAllowedServerGroup(cl) {
+		var na_ids = config.na_groups.split(",");
+		var group;
+
+		for (var index in cl.getServerGroups()) {
+
+			group = cl.getServerGroups()[index];
+
+			if(na_ids instanceof Array) {
+				for (var id in na_ids) {
+					if(na_ids[id] == group.id()) {
+						cl.chat("Du bist in der Servergruppe [color=red]" + group.name() + "[/color], deshalb darfst du den [color=blue]Ruhe[/color] Command noch nicht nutzen!");
+						return true;
+					}
+				}
+			} else {
+				if(na_ids == group.id()) {
+					cl.chat("Du bist in der Servergruppe [color=red]" + group.name() + "[/color], deshalb darfst du den [color=blue]Ruhe[/color] Command noch nicht nutzen!");
+					return true;
+				}
+			}
+
+		}
+
+		return false;
+	}
 
 });
